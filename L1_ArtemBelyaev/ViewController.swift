09144a135f09a147ea6edec03b03dc826a91356c
Артем Б <artemb@MacBook-Pro-Artem.local>
2017-09-14 20:46:10 +0500

    Initial Commit

---
+++ L1_ArtemBelyaev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  L1_ArtemBelyaev
+//
+//  Created by Артем Б on 14.09.17.
+//  Copyright © 2017 Артем Б. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
